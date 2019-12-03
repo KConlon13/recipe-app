981519443dbbbd700d91629baa13ea289311e1cc
KConlon13 <KyleConlon13@gmail.com>
2019-12-03 13:53:34 -0500

    added nav bar toggle button with routes and additional styling for navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,15 +11,40 @@ function Navbar(){
 
     return (
         <nav className="nav">
+
+            <div class="pos-f-t">
+                <div class="collapse" id="navbarToggleExternalContent">
+                    <div class="bg p-4">
+                        
+                        <ul className="nav-links">
+                            <Link style={navStyle} to="/">
+                                <p>Home</p>
+                            </Link>
+                            <Link style={navStyle} to="/about">
+                                <p>About</p>
+                            </Link>
+                        </ul>
+
+                    </div>
+                </div>
+                <nav class="navbar navbar-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                </nav>
+            </div>
+            
             <h1 id="title" >THE COOK NOOK</h1>
-            <ul className="nav-links">
+
+            {/* <ul className="nav-links">
                     <Link style={navStyle} to="/">
                         <p>Home</p>
                     </Link>
                     <Link style={navStyle} to="/about">
                         <p>About</p>
                     </Link>
-            </ul>
+            </ul> */}
+
         </nav>
     )
 }
